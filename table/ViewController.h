99ecb73ee85ff09a8fc64c1c447a22b46e547383
Мариам Б <Mia@Marimac.local>
2015-04-05 16:01:43 +0300

    Initial Commit

---
+++ table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  table
+//
+//  Created by Мариам Б. on 05.04.15.
+//  Copyright (c) 2015 Мариам Б. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ table/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  table
+//
+//  Created by Мариам Б. on 05.04.15.
+//  Copyright (c) 2015 Мариам Б. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
