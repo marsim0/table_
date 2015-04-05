59b515929dfddeb5d6c640a877661b74fa4a02e7
marsim0 <marb@mail.ru>
2015-04-05 22:08:20 +0300

    df
    dd

--- table/ViewController.m
+++ table/ViewController.m
@@ -10,12 +10,31 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSArray * rusWords;
+@property (nonatomic, strong) NSArray * engWords;
+@property (nonatomic, strong) NSMutableArray * mutable;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString * stringRusWords = @"тетрадь,собака,ложка,дом";
+    NSString * stringEngWords = @"notebook,dog,spoon,house";
+    
+    self.rusWords = [stringRusWords componentsSeparatedByString:@","];
+    self.engWords = [stringEngWords componentsSeparatedByString:@","];
+    self.mutable = [NSMutableArray array];
+    
+    for (int i = 0; i < self.rusWords.count; i++) {
+        NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                               [self.rusWords objectAtIndex: i ], @"r",
+                               [self.engWords objectAtIndex: i ], @"e", nil];
+        [self.mutable addObject:dict];
+    }
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +42,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITableViewDelegate
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+        
+    return self.mutable.count ;
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString * simpleTableIdentifier = @"Cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [[self.mutable objectAtIndex:indexPath.row]valueForKey:@"r"];
+    cell.detailTextLabel.text = [[self.mutable objectAtIndex:indexPath.row]valueForKey:@"e"];
+    
+    return cell;
+}
+
 
 @end
